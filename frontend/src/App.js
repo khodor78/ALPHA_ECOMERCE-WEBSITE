7cfd34fe237de17065fcbad52ed19221e32cd1d6
khoder habbal <khoderhabbal36@gmail.com>
2022-08-08 11:46:50 +0300

    CheckoutSteps:Addres-payment(with redux)-orderScreen(without backend) and action=> only fetchinf data from redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,11 @@ import { Route, Link, BrowserRouter } from "react-router-dom";
 import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
@@ -57,6 +60,11 @@ function App() {
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+
+
 
        </main>
         <footer className="row center">All right reserved</footer>
